5c6e41f14743d694f165b6f20298d2b23349f480
struk49 <andrewstruk@yahoo.co.uk>
2023-11-20 14:46:48 +0000

    added contacts to database

--- contact/models.py
+++ contact/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    full_name = models.CharField(max_length=255)
+    full_name = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField()
+    message = models.CharField(max_length=500)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self, ):
+        return "Message from: " + str(self.email)
